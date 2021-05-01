5eaeb78cbf3e8d22e43dcee7cea07685feb687ac
Jonathan Akwetey <okinejonathan@yahoo.com>
2021-05-01 17:48:43 +0100

    connect to api and fetch data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,40 @@
 <template>
   <div class="container" style="margin: 70px auto">
     <Breadcrumb :title="title" :breadcrumb="breadcrumb" />
-    <div class="card shadow-lg border-0">
+    <div class="col-md-6 mb-5 pl-0">
+      <input type="search" class="form-control" placeholder="search" />
+    </div>
+    <div
+      class="card shadow-lg border-0 mt-4"
+      v-for="(item, i) in items"
+      :key="i"
+    >
       <div class="card-body">
-        <h4>Hello</h4>
+        <div class="row">
+          <div class="col-md-2">
+            <img
+              :src="item.owner.avatar_url"
+              class="image-fluid"
+              width="150"
+              height="150"
+            />
+          </div>
+          <div class="col-md-10">
+            <h5>{{ item.name }}</h5>
+            <p>{{ item.description }}</p>
+            <div class="d-flex">
+              <span
+                class="badge badge-secondary mr-3 d-flex justify-content-center align-items-center"
+                >Stars: {{ item.stargazers_count }}</span
+              >
+              <span
+                class="badge badge-secondary mr-3 d-flex justify-content-center align-items-center"
+                >Issues: {{ item.open_issues_count }}</span
+              >
+              <span>30 days by {{ item.owner.login }}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -21,7 +52,7 @@ export default defineComponent({
   },
   data() {
     return {
-      title: "Trending Repos" as string,
+      title: "Trending Repos Last 30 Days" as string,
       breadcrumb: [
         {
           name: "Home",
@@ -33,6 +64,7 @@ export default defineComponent({
           isActive: true,
         },
       ] as BreadcrumbItems[],
+      items: [],
     };
   },
 
@@ -40,6 +72,7 @@ export default defineComponent({
     try {
       let res = await TrendingRepoService.index();
       console.log(res);
+      this.items = res.data.items;
     } catch (error) {
       console.log(error);
     }
